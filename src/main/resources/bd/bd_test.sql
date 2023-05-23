3f5b8c60018b19fee5df708b215a1ff6877c7de9
CarlosEGB <50418087+CarlosEGB@users.noreply.github.com>
2023-05-23 18:42:10 -0500

    Update bd_test.sql
    se actualizo la bd de test

--- src/main/resources/bd/bd_test.sql
+++ src/main/resources/bd/bd_test.sql
@@ -1,3 +1,38 @@
+CREATE DATABASE quizpro_bd;
+
+USE quizpro_bd;
+
+CREATE TABLE questions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  question TEXT NOT NULL
+);
+
+CREATE TABLE answers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  answer TEXT NOT NULL,
+  is_correct BOOLEAN NOT NULL,
+  question_id INT NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dni VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE results (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  question_id INT NOT NULL,
+  answer_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (answer_id) REFERENCES answers(id)
+);
+
 INSERT INTO questions (question) VALUES
 ('<p>Con pruebas exhaustivas es posible eliminar todos los defectos de un programa antes de su entrega al cliente.&nbsp;</p>'),
 ('<p>La diferencia entre la repetici&oacute;n de pruebas y las pruebas de regresi&oacute;n es la siguiente&nbsp;</p>'),
