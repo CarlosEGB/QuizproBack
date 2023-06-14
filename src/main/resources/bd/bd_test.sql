186a3ce390d2955e8476c921a611472146f2ab0f
CarlosGarcia <carlos_9655@hotmail.com>
2023-06-13 23:17:17 -0500

    Se actualizo bd test

--- src/main/resources/bd/bd_test.sql
+++ src/main/resources/bd/bd_test.sql
@@ -2,7 +2,7 @@ INSERT INTO questions (question) VALUES
 ('<p>Con pruebas exhaustivas es posible eliminar todos los defectos de un programa antes de su entrega al cliente.&nbsp;</p>'),
 ('<p>La diferencia entre la repetici&oacute;n de pruebas y las pruebas de regresi&oacute;n es la siguiente&nbsp;</p>'),
 ('<p>Dada la siguiente tabla de transiciones de estado &iquest;Cu&aacute;l de los siguientes casos de prueba cubrir&aacute; la siguiente serie de transiciones de estado? S1 SO S1 S2 SO</p>
-<p><img src="https://ckeditor.com/apps/ckfinder/userfiles/files/image-20230506200706-1.png" style="height:114px; width:217px" /></p>');
+<p><img src="/src/assets/imgs/id_1.png" style="height:114px; width:217px" /></p>');
 
 INSERT INTO answers (answer, is_correct, question_id) VALUES
 ('Verdadero',false,1),
